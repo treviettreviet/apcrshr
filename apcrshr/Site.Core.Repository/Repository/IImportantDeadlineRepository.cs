06da4139e2b5a902f64d0b357d7c2b2b6bfdc5b2
trangdoan1610 <doantrang483@gmail.com>
2016-02-23 09:42:40 +0700

    Important Deadline interface

---
+++ apcrshr/Site.Core.Repository/Repository/IImportantDeadlineRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Core.Repository.Repository
+{
+    public interface IImportantDeadlineRepository
+    {
+        ImportantDeadline FindByActionURL(string actionURL);
+        ImportantDeadline FindByID(object id);
+        ImportantDeadline FindByTitle(string title);
+        IList<ImportantDeadline> FindTop(int top);
+        IList<ImportantDeadline> Search(string key);
+        IList<ImportantDeadline> FindAll();
+        Tuple<int, IList<ImportantDeadline>> FindAll(int pageSize, int pageIndex);
+    }
+}
