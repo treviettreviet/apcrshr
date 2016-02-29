71d1af899722efb2ba6f9fae923dccc15ebf15f3
ducanhz <anhndgc00893@fpt.edu.vn>
2016-02-29 10:17:38 +0700

    ConferenceData

---
+++ apcrshr/Site.Core.DataModel/Model/ConferenceDataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Site.Core.DataModel.Model
+{
+    public class ConferenceDataModel
+    {
+        public string ConferenceDataID { get; set; }
+        public string Title { get; set; }
+        [AllowHtml]
+        public string Contents { get; set; }
+        public string ActionURL { get; set; }
+        public string CreatedBy { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public Nullable<System.DateTime> UpdatedDate { get; set; }
+        public string Language { get; set; }
+        public string MenuID { get; set; }
+        public string DocURL { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
---
+++ apcrshr/Site.Core.Repository/Implementation/ConferenceDataRepository.cs
@@ -0,0 +1,72 @@
+﻿using Site.Core.Repository.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Core.Repository.Implementation
+{
+    public class ConferenceDataRepository : IConferenceDataRepository
+    {
+        public ConferenceData FindByActionURL(string actionURL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ConferenceData> FindTop(int top, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, IList<ConferenceData>> FindAll(int pageSize, int pageIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, IList<ConferenceData>> FindAll(int pageSize, int pageIndex, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, IList<ConferenceData>> FindAll(int pageSize, int pageIndex, string language, string menuID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<int, IList<ConferenceData>> FindAllRelated(DateTime date, int pageSize, int pageIndex, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ConferenceData> Search(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Insert(ConferenceData item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ConferenceData item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ConferenceData FindByID(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<ConferenceData> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
