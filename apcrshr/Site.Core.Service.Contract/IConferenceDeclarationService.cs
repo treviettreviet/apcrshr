c8b8d40d26881385271da6b24ed3e18562341105
ducanhz <anhndgc00893@fpt.edu.vn>
2016-02-29 13:05:53 +0700

    conference service

---
+++ apcrshr/Site.Core.Service.Contract/IConferenceDeclarationService.cs
@@ -0,0 +1,62 @@
+﻿using Site.Core.DataModel.Model;
+using Site.Core.DataModel.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Core.Service.Contract
+{
+    public interface IConferenceDeclarationService
+    {
+        /// <summary>
+        /// Find ConferenceDeclaration by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        FindItemReponse<ConferenceDeclarationModel> FindConferenceByID(string id);
+
+        /// <summary>
+        /// Delete ConferenceDeclaration
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        BaseResponse DeleteConference(string id);
+
+        /// <summary>
+        /// Update ConferenceDeclaration
+        /// </summary>
+        /// <param name="Conference"></param>
+        /// <returns></returns>
+        BaseResponse UpdateConference(ConferenceDeclarationModel conference);
+
+        /// <summary>
+        /// Get all ConferenceDeclaration
+        /// </summary>
+        /// <returns></returns>
+        FindAllItemReponse<ConferenceDeclarationModel> GetConference();
+
+        /// <summary>
+        /// Get ConferenceDeclaration with paging
+        /// </summary>
+        /// <param name="pageSize">int</param>
+        /// <param name="pageIndex">int</param>
+        /// <returns></returns>
+        FindAllItemReponse<ConferenceDeclarationModel> GetConference(int pageSize, int pageIndex);
+
+        /// <summary>
+        /// Create ConferenceDeclaration
+        /// </summary>
+        /// <param name="Conference"></param>
+        /// <returns></returns>
+        InsertResponse CreateConference(ConferenceDeclarationModel conference);
+
+        /// <summary>
+        /// Get ConferenceDeclaration by ID
+        /// </summary>
+        /// <param name="conferenceID"></param>
+        /// <returns></returns>
+        FindItemReponse<ConferenceDeclarationModel> GetConferenceByID(string conferenceID);
+    }
+}
---
+++ apcrshr/Site.Core.Service.Implementation/ConferenceDeclarationService.cs
@@ -0,0 +1,47 @@
+﻿using Site.Core.Service.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Site.Core.Service.Implementation
+{
+    public class ConferenceDeclarationService : IConferenceDeclarationService
+    {
+        public DataModel.Response.FindItemReponse<DataModel.Model.ConferenceDeclarationModel> FindConferenceByID(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataModel.Response.BaseResponse DeleteConference(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataModel.Response.BaseResponse UpdateConference(DataModel.Model.ConferenceDeclarationModel conference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataModel.Response.FindAllItemReponse<DataModel.Model.ConferenceDeclarationModel> GetConference()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataModel.Response.FindAllItemReponse<DataModel.Model.ConferenceDeclarationModel> GetConference(int pageSize, int pageIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataModel.Response.InsertResponse CreateConference(DataModel.Model.ConferenceDeclarationModel conference)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataModel.Response.FindItemReponse<DataModel.Model.ConferenceDeclarationModel> GetConferenceByID(string conferenceID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
