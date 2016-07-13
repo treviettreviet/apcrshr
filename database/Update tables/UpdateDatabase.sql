ccfea468b58d78b6e2e501407c8476306d293b8c
Mau Tien Doan <tiendoan246@bitbucket.org>
2016-07-13 09:38:15 +0700

    user table changes

--- database/Update tables/UpdateDatabase.sql
+++ database/Update tables/UpdateDatabase.sql
@@ -118,3 +118,115 @@ GO
 ALTER TABLE [Article] ADD [HomeDisplay] BIT NOT NULL DEFAULT(0)
 
 GO
+
+ALTER TABLE [User] ADD [MealPreference] VARCHAR(50) NULL
+
+GO
+
+ALTER TABLE [User] ADD [DisabilitySpecialTreatment] NVARCHAR(500) NULL
+
+GO
+
+ALTER TABLE [User] ALTER COLUMN [UserName] VARCHAR(100) NOT NULL
+
+GO
+
+DROP TABLE [OfficeAddress]
+
+GO
+
+ALTER TABLE [User] ADD [Address] NVARCHAR(200) NULL
+
+GO
+
+ALTER TABLE [User] ADD [City] VARCHAR(50) NULL
+
+GO
+
+ALTER TABLE [User] ADD [Country] VARCHAR(50) NULL
+
+GO
+
+ALTER TABLE [User] ADD [WorkAddress] NVARCHAR(200) NULL
+
+GO
+
+ALTER TABLE [User] ADD [Organization] NVARCHAR(200) NULL
+
+GO
+
+ALTER TABLE [MailingAddress] DROP COLUMN [AddressLine1]
+
+GO
+
+ALTER TABLE [MailingAddress] DROP COLUMN [City]
+
+GO
+
+ALTER TABLE [MailingAddress] DROP COLUMN [StateProvince]
+
+GO
+
+ALTER TABLE [MailingAddress] DROP COLUMN [Country]
+
+GO
+
+ALTER TABLE [MailingAddress] DROP COLUMN [PostalCode]
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [ParticipantType] VARCHAR(100) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [ParticipateYouth] VARCHAR(50) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [DateOfBirth] DATETIME NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [OriginalNationality] VARCHAR(200) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [CurrentNationality] VARCHAR(200) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [PassportNumber] VARCHAR(50) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [TypeOfPassport] VARCHAR(50) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [Occupation] VARCHAR(200) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [DateOfPassportIssue] DATETIME NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [DateOfPassportExpiry] DATETIME NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [PassportPhoto1] NVARCHAR(200) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [PassportPhoto2] NVARCHAR(200) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [PassportPhoto3] NVARCHAR(200) NOT NULL
+
+GO
+
+ALTER TABLE [MailingAddress] ADD [DetailOfEmbassy] NVARCHAR(MAX) NULL
+
+GO
